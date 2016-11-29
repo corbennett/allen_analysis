9ec1b8303d18b32a9eee2cc5b48047026ef18240
Sam Gale <samueldgale@gmail.com>
2016-11-28 17:25:06 -0800

    Add files via upload

--- LaserControl.py
+++ LaserControl.py
@@ -38,14 +38,13 @@ class LaserControlGUI():
         mainWinRect.moveCenter(screenCenter)
         self.mainWin.move(mainWinRect.topLeft())
         
-        self.nidaqDigOut = nidaq.DigitalOutputs(device='Dev1',port=1,initialState='high')
-        self.nidaqDigOut.StartTask()
-        self.nidaqDigOut.Write(self.nidaqDigOut.lastOut)
+        self.nidaqDigitalOut = nidaq.DigitalOutputs(device='Dev1',port=1,initialState='high')
+        self.nidaqDigitalOut.StartTask()
+        self.nidaqDigitalOut.Write(self.nidaqDigitalOut.lastOut)
         
-        self.blueLaser = LaserControlObj('Blue Laser','COM4')
+        self.blueLaser = LaserControlObj(app,'Blue Laser','COM4')
         
-        self.orangeLaser =LaserControlObj('Orange Laser','COM5',shutterControl='digital',nidaqDigOut=self.nidaqDigOut)
-        
+        self.orangeLaser = LaserControlObj(app,'Orange Laser','COM5',shutterControlMode='digital',nidaqDigitalOut=self.nidaqDigitalOut,ch=1)
         
         setLayoutGridSpacing(self.blueLaser.layout,winHeight/2,winWidth,5,3)
         setLayoutGridSpacing(self.orangeLaser.layout,winHeight/2,winWidth,5,3)
@@ -64,14 +63,16 @@ class LaserControlGUI():
         self.blueLaser.nidaqAnalogOut.StopTask()
         self.blueLaser.nidaqAnalogOut.ClearTask()
         self.orangeLaser.serialPort.close()
-        self.orangeLaser.nidaqDigOut.StopTask()
-        self.orangeLaser.nidaqDigOut.ClearTask()
+        self.orangeLaser.nidaqDigitalOut.StopTask()
+        self.orangeLaser.nidaqDigitalOut.ClearTask()
         event.accept()
     
 
 class LaserControlObj():
     
-    def __init__(self,label,port,shutterControl='analog',nidaqDigOut=None):
+    def __init__(self,app,label,port,shutterControlMode='analog',nidaqDigitalOut=None,ch=0):
+        self.app = app        
+        
         self.label = QtGui.QLabel(label)
         self.label.setAlignment(QtCore.Qt.AlignHCenter)
         
@@ -82,25 +83,28 @@ class LaserControlObj():
         self.serialPort.stopbits = serial.STOPBITS_ONE
         self.serialPort.parity = serial.PARITY_NONE
         self.serialPort.open()
-        if shutterControl=='analog':
-            self.serialPort.write('cp\r')
-            self.serialPort.write('p 0\r')
-        
-        self.shutterControl = shutterControl
-        if shutterControl=='digital':
-            self.nidaqDigOut = nidaqDigOut
-            self.nidaqDigOutCh = 1
-        else:
-            self.nidaqAnalogOut = nidaq.AnalogOutput(device='Dev1',channel=0,voltageRange=(0,5))
-            self.nidaqAnalogOut.StartTask()
         
         self.powerControl = QtGui.QDoubleSpinBox()
-        self.powerControl.setPrefix('Power:  ')
-        self.powerControl.setSuffix(' mW')
-        self.powerControl.setDecimals(1)
-        self.powerControl.setRange(0,100)
-        self.powerControl.setSingleStep(0.5)
-        self.powerControl.setValue(100)
+        self.shutterControlMode = shutterControlMode
+        if shutterControlMode=='digital':
+            self.nidaqDigitalOut = nidaqDigitalOut
+            self.nidaqDigitalOutCh = ch
+            self.powerControl.setPrefix('Power:  ')
+            self.powerControl.setSuffix(' mW')
+            self.powerControl.setDecimals(1)
+            self.powerControl.setRange(0,100)
+            self.powerControl.setSingleStep(0.5)
+            self.powerControl.setValue(100)
+        else:
+            self.serialPort.write('em\r sdmes 0\r sames 1\r') # analog modulation mode
+            self.nidaqAnalogOut = nidaq.AnalogOutput(device='Dev1',channel=ch,voltageRange=(0,5))
+            self.nidaqAnalogOut.StartTask()
+            self.nidaqAnalogOut.Write(np.array([0.0]))
+            self.powerControl.setSuffix(' V')
+            self.powerControl.setDecimals(2)
+            self.powerControl.setRange(0,1.5)
+            self.powerControl.setSingleStep(0.05)
+            self.powerControl.setValue(1)
         self.powerControl.valueChanged.connect(self.powerControlChanged)
         
         self.modeMenu = QtGui.QComboBox()
@@ -135,7 +139,7 @@ class LaserControlObj():
         self.pulseIntervalControl.valueChanged.connect(self.pulseIntChanged)
         self.pulseIntervalControl.setEnabled(False)
         
-        self.onOffButton = QtGui.QPushButton('Laser On',checkable=True)
+        self.onOffButton = QtGui.QPushButton('Start',checkable=True)
         self.onOffButton.clicked.connect(self.onOffButtonPress)
         
         self.layout = QtGui.QGridLayout()
@@ -156,25 +160,10 @@ class LaserControlObj():
             self.pulseNumControl.setEnabled(False)
             self.pulseIntervalControl.setEnabled(False)
             self.pulseDurControl.setEnabled(False)
-            if self.shutterControl=='analog':
-                self.powerControl.setSuffix(' mW')
-                self.powerControl.setDecimals(1)
-                self.powerControl.setRange(0,100)
-                self.powerControl.setSingleStep(0.5)
-                self.powerControl.setValue(100)
-                self.serialPort.write('cp\r') # constant power mode
-                self.serialPort.write('p 0\r')
         else:
             self.pulseNumControl.setEnabled(True)
             self.pulseIntervalControl.setEnabled(True)
             self.pulseDurControl.setEnabled(True)
-            if self.shutterControl=='analog':
-                self.powerControl.setSuffix(' V')
-                self.powerControl.setDecimals(2)
-                self.powerControl.setRange(0,1.5)
-                self.powerControl.setSingleStep(0.05)
-                self.powerControl.setValue(1)
-                self.serialPort.write('em\r sdmes 0\r sames 1\r') # analog modulation mode
     
     def pulseNumChanged(self,val):
         pass
@@ -187,33 +176,40 @@ class LaserControlObj():
     
     def onOffButtonPress(self,val):
         if self.onOffButton.isChecked():
-            self.onOffButton.setText('Laser Off')
+            self.onOffButton.setText('Stop')
             if self.modeMenu.currentIndex()==0:
-                if self.shutterControl=='digital':
-                    self.nidaqDigOut.WriteBit(self.nidaqDigOutCh,0)
+                if self.shutterControlMode=='digital':
+                    self.nidaqDigitalOut.WriteBit(self.nidaqDigitalOutCh,0)
                 else:
-                    self.serialPort.write('p '+str(self.powerControl.value()/1e3)+'\r')
+                    self.nidaqAnalogOut.Write(np.array([self.powerControl.value()]))
             else:
-                if self.shutterControl=='digital':
-                    for i in range(self.pulseNumControl.value()):
-                        if i>0:
-                            time.sleep(self.pulseIntervalControl.value())
-                        self.nidaqDigOut.WriteBit(self.nidaqDigOutCh,0)
+                for i in range(self.pulseNumControl.value()):
+                    if i>0:
+                        time.sleep(self.pulseIntervalControl.value())
+                    self.app.processEvents()
+                    if not self.onOffButton.isChecked():
+                        return
+                    if self.shutterControlMode=='digital':
+                        self.nidaqDigitalOut.WriteBit(self.nidaqDigitalOutCh,0)
                         time.sleep(self.pulseDurControl.value())
-                        self.nidaqDigOut.WriteBit(self.nidaqDigOutCh,1)
-                else:
-                    pulseSamples = round(self.nidaqAnalogOut.sampRate*self.pulseDurControl.value())
-                    intervalSamples = round(self.nidaqAnalogOut.sampRate*self.pulseIntervalControl.value())
-                    pulseTrain = np.zeros((self.pulseNumControl.value(),pulseSamples+intervalSamples))
-                    pulseTrain[:,:pulseSamples] = self.powerControl.value()
-                    self.nidaqAnalogOut.Write(pulseTrain.ravel()[:-intervalSamples+1])
+                        self.nidaqDigitalOut.WriteBit(self.nidaqDigitalOutCh,1)
+                    else:
+                        self.nidaqAnalogOut.Write(np.array([self.powerControl.value()]))
+                        time.sleep(self.pulseDurControl.value())
+                        self.nidaqAnalogOut.Write(np.array([0.0]))
                 self.onOffButton.click()
         else:
-            self.onOffButton.setText('Laser On')
-            if self.shutterControl=='digital':
-                self.nidaqDigOut.WriteBit(self.nidaqDigOutCh,1)
-            elif self.modeMenu.currentIndex()==0:
-                self.serialPort.write('p 0\r')
+            self.onOffButton.setText('Start')
+            if self.shutterControlMode=='digital':
+                self.nidaqDigitalOut.WriteBit(self.nidaqDigitalOutCh,1)
+            else:
+                self.nidaqAnalogOut.Write(np.array([0.0]))
+                
+# pulseSamples = round(self.nidaqAnalogOut.sampRate*self.pulseDurControl.value())
+# intervalSamples = round(self.nidaqAnalogOut.sampRate*self.pulseIntervalControl.value())
+# pulseTrain = np.zeros((self.pulseNumControl.value(),pulseSamples+intervalSamples))
+# pulseTrain[:,:pulseSamples] = self.powerControl.value()
+# self.nidaqAnalogOut.Write(pulseTrain.ravel()[:-intervalSamples+1])
     
 
 def setLayoutGridSpacing(layout,height,width,rows,cols):
